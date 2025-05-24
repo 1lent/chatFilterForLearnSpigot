1cd9768ecbf18e24ec5843dd39022bc7db65caf9
1lent <brodiecuff@outlook.com>
2025-05-24 15:19:00 +0800

    fix 3

--- src/main/java/com/lent/chatFiltere/CfEvent.java
+++ src/main/java/com/lent/chatFiltere/CfEvent.java
@@ -4,12 +4,10 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-
 import java.util.List;
 
 public class CfEvent implements Listener {
     private final ChatFilter plugin;
-
     public CfEvent(ChatFilter plugin) {
         this.plugin = plugin;
     }
--- src/main/java/com/lent/chatFiltere/ChatFilter.java
+++ src/main/java/com/lent/chatFiltere/ChatFilter.java
@@ -3,11 +3,7 @@ package com.lent.chatFiltere;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.List;
-
 public final class ChatFilter extends JavaPlugin {
-    private List<String> bannedWords;
-
     @Override
     public void onEnable() {
         saveDefaultConfig();
